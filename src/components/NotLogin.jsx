6279b050bb9e88f62f1afa28ca7546c50c560f17
arnuphapt <137035651+arnuphapt@users.noreply.github.com>
2024-03-17 17:35:43 +0700

    Delete

--- src/components/NotLogin.jsx
+++
@@ -1,7 +0,0 @@
-export default function NotLogin() {
-    return (
-        <div className="min-h-dvh flex items-center justify-center">
-            <h1>กรุณาเข้าสู่ระบบ</h1>
-        </div>
-    )
-}
